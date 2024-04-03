e789099ae81b7ebe255f9c5cd3bfe4a482384560
lordfiftyfive <subarnos@live.com>
2024-04-02 23:38:37 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,43 @@ from os import path
 
 import setuptools
 
+
+"""
+from setuptools import setup, find_packages
+
+# Parse README
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="strnn",
+    version="0.2.0",
+    author="RGKLab",
+    description="PyTorch package for Structured Neural Networks.",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    url="https://github.com/rgklab/StructuredNNs",
+    install_requires=[
+        "torch>=2.0.0",
+        "scikit-learn",
+        "matplotlib",
+        "torchdiffeq==0.2.3",
+        "UMNN",
+        "wandb"
+    ],
+    python_requires='>=3.11',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    packages=find_packages(
+        where=".",
+        exclude=["experiments*", "data*", "test*"]
+    ),
+)
+
+"""
 path_to_repo = path.abspath(path.dirname(__file__))
 with open(path.join(path_to_repo, 'readme.md'), encoding='utf-8') as f:
     long_description = f.read()
